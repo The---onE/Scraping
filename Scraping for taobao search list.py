0785796cb89555cf748e5205cfd6f8b48ff14706
The-_-onE <834489218@qq.com>
2015-12-02 22:23:42 +0800

    Save Much information from Taobao Search list

--- Scraping for taobao search list.py
+++ Scraping for taobao search list.py
@@ -20,6 +20,7 @@ def consoleoutput(string):
     return string.decode('utf-8').encode('gbk')
     # return string
 
+
 def fileoutput(string):
     return string.decode('utf-8').encode('gbk')
 
@@ -33,19 +34,27 @@ def gethtml(url, data, headers):
 
 def saveinformation(file, inf, index, page):
     id = inf.group(1).replace(',', '.')
-    title = inf.group(2).replace(',', '.')
-    picture = inf.group(3).replace(',', '.')
-    price = inf.group(4).replace(',', '.')
-    order = inf.group(5).replace(',', '.')
-    codeinf = '%s' % index + '|' + id + '|' + title + '|' + picture + '|' + price + '|' + order + '|' + '%s' % page
+    category = inf.group(2).replace(',', '.')
+    title = inf.group(3).replace(',', '.')
+    picture = inf.group(4).replace(',', '.')
+    price = inf.group(5).replace(',', '.')
+    location = inf.group(6).replace(',', '.')
+    order = inf.group(7).replace(',', '.')
+    if inf.group(8)  == None:
+        comment = '0'
+    else:
+        comment = inf.group(8).replace(',', '.')
+    shop = inf.group(9).replace(',', '.')
+
+    codeinf = '%s' % index + '|' + id + '|' + category + '|' + title + '|' + picture + '|' + price + '|' + location + '|' + order + '|' + comment + '|' + shop + '|' + '%s' % page
     print consoleoutput(codeinf)
 
-    fileinf = '%s' % index + ',' + id + ',' + title + ',' + picture + ',' + price + ',' + order + ',' + '%s' % page + '\n'
+    fileinf = '%s' % index + ',' + id + ',' + category + ',' + title + ',' + picture + ',' + price + ',' + location + ',' + order + ',' + comment + ',' + shop + ',' + '%s' % page + '\n'
     file.write(fileoutput(fileinf))
 
 
 def getinformation(file, html, page):
-    reg = r'"nid":"(.+?)",[\s\S]*?raw_title":"([\s\S]+?)","pic_url":"//(.+?)"[\s\S]*?view_price":"(.+?)"[\s\S]*?sales":"(.+?)人付款"'
+    reg = r'"nid":"(.*?)","category":"(.*?)"[\s\S]*?raw_title":"([\s\S]*?)","pic_url":"//(.+?)"[\s\S]*?view_price":"(.*?)"[\s\S]*?item_loc":"(.*?)"[\s\S]*?sales":"(.*?)人付款"(?:,"comment_count":"([0-9]+?)")*[\s\S]+?nick":"(.*?)"'
     infre = re.compile(reg)
     inflist = infre.finditer(html)
     flag = False
