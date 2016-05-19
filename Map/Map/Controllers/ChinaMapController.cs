cc500ad5472b3b38888cfb3116a93e84fce94813
The-_-onE <834489218@qq.com>
2016-05-19 22:59:10 +0800

    Could show the Province Area of China

---
+++ Map/Map/Controllers/ChinaMapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Map.Controllers
+{
+    public class ChinaMapController : Controller
+    {
+        //
+        // GET: /ChinaMap/
+
+        public ActionResult LandArea()
+        {
+            return View();
+        }
+
+    }
+}
